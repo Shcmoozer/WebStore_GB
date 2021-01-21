6955d8a74c83fb97efc2df04cf3fb2011565f87c
Nika.P <pns9595@mail.ru>
2021-01-21 23:02:21 +0300

    Отображение сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -9,50 +9,6 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> __Employees = new()
-        {
-            new Employee
-            {
-                Id = 1,
-                LastName = "Иванов",
-                FirstName = "Иван",
-                Patronymic = "Иванович",
-                Age = 37,
-                Phone = "79097897986",
-                Salary = 5700
-            },
-            new Employee
-            {
-                Id = 2,
-                LastName = "Петров",
-                FirstName = "Петр",
-                Patronymic = "Петрович",
-                Age = 19,
-                Phone = "79385238515",
-                Salary = 3200
-            },
-            new Employee
-            {
-                Id = 3,
-                LastName = "Сидоров",
-                FirstName = "Сидор",
-                Patronymic = "Сидорович",
-                Age = 26,
-                Phone = "79236358565",
-                Salary = 7300
-            }
-        };
-
-        public IActionResult Index() => View(/*"SecondView"*/);
-
-        public IActionResult SecondAction()
-        {
-            return Content("Second controller action");
-        }
-
-        public IActionResult Employees()
-        {
-            return View(__Employees);
-        }
+        public IActionResult Index() => View();
     }
 }
---
+++ WebStore/Data/TestData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public static class TestData
+    {
+        public static  List<Employee> Employees { get; } = new ()
+        {
+            new Employee
+            {
+                Id = 1,
+                LastName = "Иванов",
+                FirstName = "Иван",
+                Patronymic = "Иванович",
+                Age = 37,
+                Phone = "79097897986",
+                Salary = 5700
+            },
+            new Employee
+            {
+                Id = 2,
+                LastName = "Петров",
+                FirstName = "Петр",
+                Patronymic = "Петрович",
+                Age = 19,
+                Phone = "79385238515",
+                Salary = 3200
+            },
+            new Employee
+            {
+                Id = 3,
+                LastName = "Сидоров",
+                FirstName = "Сидор",
+                Patronymic = "Сидорович",
+                Age = 26,
+                Phone = "79236358565",
+                Salary = 7300
+            }
+        };
+    }
+}
