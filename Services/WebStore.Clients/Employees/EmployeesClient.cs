58a429629802ab6c53897261424225071303a009
Nika.P <pns9595@mail.ru>
2021-04-18 13:03:23 +0300

    Клиент для работы с WebAPI управления сотрудниками

--- Services/WebStore.Clients/Employees/EmployeesClient.cs
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -9,9 +9,36 @@ using WebStore.Interfaces.Services;
 
 namespace WebStore.Clients.Employees
 {
-    public class EmployeesClient : BaseClient
+    public class EmployeesClient : BaseClient, IEmployeesData
     {
-        public EmployeesClient(IConfiguration Configuration) : base(Configuration, WebAPI.Employees) { }
+        private readonly ILogger<EmployeesClient> _Logger;
 
+        public EmployeesClient(IConfiguration Configuration, ILogger<EmployeesClient> Logger)
+            : base(Configuration, WebAPI.Employees) =>
+            _Logger = Logger;
+
+        public IEnumerable<Employee> Get() => Get<IEnumerable<Employee>>(Address);
+
+        public Employee Get(int id) => Get<Employee>($"{Address}/{id}");
+
+        public Employee GetByName(string LastName, string FirstName, string Patronymic) =>
+            Get<Employee>($"{Address}/employee?LastName={LastName}&FirstName={FirstName}&Patronymic={Patronymic}");
+
+        public int Add(Employee employee) => Post(Address, employee).Content.ReadAsAsync<int>().Result;
+
+        public Employee Add(string LastName, string FirstName, string Patronymic, int Age) =>
+            Post($"{Address}/employee?LastName={LastName}&FirstName={FirstName}&Patronymic={Patronymic}", "")
+                .Content.ReadAsAsync<Employee>().Result;
+
+        public void Update(Employee employee) => Put(Address, employee);
+
+        public bool Delete(int id)
+        {
+            _Logger.LogInformation("Удаление сотрудника id:{0}...", id);
+            var result = Delete($"{Address}/{id}").IsSuccessStatusCode;
+            _Logger.LogInformation("Удаление сотрудника id:{0} - {1}",
+                id, result ? "выполнено" : "не найден");
+            return result;
+        }
     }
 }
