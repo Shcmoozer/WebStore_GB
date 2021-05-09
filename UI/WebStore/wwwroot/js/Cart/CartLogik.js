8fdffce42576717c443cc469ac337193a74f233f
Nika.P <pns9595@mail.ru>
2021-05-09 12:58:06 +0300

    Подготовка API корзины для взаимодействия со скриптом

---
+++ UI/WebStore/wwwroot/js/Cart/CartLogik.js
@@ -0,0 +1,15 @@
+﻿Cart = {
+    _properties: {
+        getCartViewLink: "",
+        addToCartLink: "",
+        decrementLink: "",
+        removeFromCartLink: ""
+    },
+
+    init: function (properties) {
+        $.extend(Cart._properties, properties);
+        Cart.initEvents();
+    },
+
+    
+}
